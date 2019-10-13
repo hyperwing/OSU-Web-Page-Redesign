e9e2eea288b8b431ec2f54de61429d51a74c0736
neelm88 <neelm88@gmail.com>
2019-10-13 19:21:09 -0400

    code review changes

--- webpage_pull/data_parse.rb
+++ webpage_pull/data_parse.rb
@@ -128,24 +128,6 @@ def publication_page (page)
      publish_information
 end
 
-# Created 10/10/2019 by Sri Ramya Dandu
-# Edited 10/11/2019 by Sri Ramya Dandu: Changed incorrect dates
-# All dates of publications
-def getDates
-    ["Soon", "OCT 2019", "OCT 2019", "FEB 2019", "OCT 2018", "SEP 2017", "2015", "SEP 2014", "AUG 2014",
-            "JUN 2014", "2013", "NOV 2012","NOV 2012", "2012", "2011","2011", "JUN 2011", "JUN 2011",
-            "JAN 2011", "NOV 2010", "NOV 2010","NOV 2010","NOV 2010","AUG 2010","AUG 2010", "2010",
-            "OCT 2009", 'JUL 2009', 'FEB 2009', 'DEC 2008','SEP 2008', 'JUN 2008', 'JUN 2008','JAN 2008',
-            'JAN 2008', 'JAN 2008', 'OCT 2007', 'JUN 2007', 'JUN 2007', '2007','2007', 'FEB 2007','FEB 2007',
-            'FEB 2007', '2007', '2007', 'OCT 2006', 'OCT 2006', '2006', 'JUN 2006', 'JUN 2006', 'MAY 2006', '2006',
-            'SEP 2005', 'SEP 2005', 'JUN 2005', 'JAN 2005', '2004', 'AUG 2004', 'JUL 2004', 'JUL 2004', 'JUN 2004',
-            '2004','2003', 'OCT 2003', 'JUL 2003', 'DEC 2002', '2002', 'AUG 2002', 'JUL 2002', '2001', 'NOV 2001',
-            'OCT 2001', 'JUL 2001', 'JUN 2001', '2000','DEC 2000', 'JUN 2000', 'NOV 2000','1999', 'MAY 1999', 'SEP 1999',
-            'MAR 1999', '1998', 'NOV 1998', 'APR 1998', 'MAR 1998', 'OCT 1997', 'JUN 1997', 'JUN 1997','1997', '1996',
-            'AUG 1996', '1994', '1994', 'OCT 1994']
-
-end
-
 # Main orchestrator for caching
 #===============================================================
 agent = Mechanize.new
